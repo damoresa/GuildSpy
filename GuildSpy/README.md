d5353173aced2529618f3a7faeb63c6e1b3d04cb
damoresa <damoresac@gmail.com>
2016-03-14 20:20:02 +0100

    - Initial commit

---
+++ GuildSpy/README.md
@@ -0,0 +1,24 @@
+
+
+# GuildSpy
+
+Small application in order to track people logging in and out on the MMO Tibia.
+
+## Usage
+
+Once the site is deployed, as soon as you get connected, you'll get notifications if the ammount of online players of any of the tracked guilds increases.
+
+## Developing
+
+Daniel Amores. Contact: damoresac@gmail.com
+
+### Tools
+
+Created with:
+
+`NodeJS`
+`Express`
+`Request`
+`Cheerio`
+`Socket.IO`
+`Cron`
---
+++ GuildSpy/server.js
@@ -0,0 +1,88 @@
+var express = require('express');
+var io = require('socket.io');
+var request = require('request');
+var cheerio = require('cheerio');
+var CronJob = require('cron').CronJob;
+
+var guildSpy = express();
+
+// Global variables
+var onlinePeople = 0;
+var onlineCheckers = 0;
+
+// Serve static content
+guildSpy.use(express.static('public'));
+
+// Mappings
+guildSpy.get('/', function(req, res) {
+	res.sendFile(__dirname + '/' + 'index.htm');
+});
+
+var scraper = function() {
+	
+	// TODO: At some point in time we could make this configurable
+	var url = 'https://secure.tibia.com/community/?subtopic=guilds&page=view&onlyshowonline=1&GuildName=Cause';
+	
+	console.log('Requesting data');
+	
+	request(url, function(error, response, html) {
+	    if(!error){
+	        var $ = cheerio.load(html);
+		    
+	        console.log('Fetching data');
+	        
+		    var onlineCheck = 0;
+		    
+		    $('td.onlinestatus')
+		    	.each(function() {
+		    		onlineCheck++;
+		    	});
+		    
+		    if (onlineCheck > onlinePeople)
+		    {
+		    	var warning = {warning: 'S'};
+		    	ioGS.emit('warning', warning);
+		    }
+		    
+		    onlinePeople = onlineCheck;
+		    
+		    console.log('Data fetched');
+		    console.log('Theres ' + onlinePeople + ' connected enemies.');
+	    }
+	});
+};
+
+//Server configuration
+var server = guildSpy.listen(8081, function() {
+
+	var host = server.address().address;
+	var port = server.address().port;
+
+	console.log("GuildSpy listening at http://%s:%s", host, port);
+	console.log("Starting CronJob");
+	
+	var job = new CronJob({
+		  cronTime: '05 * * * * *',
+		  onTick: scraper,
+		  start: false,
+		  timeZone: 'Europe/Madrid'
+		});
+	job.start();
+	
+	console.log("CronJob started");
+});
+
+//Socket.IO configuration
+var ioGS = io.listen(server);
+ioGS.on('connection', function(socket) {
+	
+	onlineCheckers++;
+	console.log('User connected');
+	console.log('Theres ' + onlineCheckers + ' connected');
+	
+	socket.on('disconnect', function() {
+		onlineCheckers--;
+		console.log('User disconneccted');
+		console.log('Theres ' + onlineCheckers + ' connected');
+	});
+});
